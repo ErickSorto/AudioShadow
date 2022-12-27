dc766e5c4e7dac70e69ee15d8805f7d453e3bd03
Erick Sorto <sortoerick33@gmail.com>
2022-12-26 21:47:13 -0500

    Updated dependencies
    Added ROOM dependencies

--- app/src/main/java/com/ballistic/audioshadow/domain/model/AudioRecording.kt
+++ app/src/main/java/com/ballistic/audioshadow/domain/model/AudioRecording.kt
@@ -1,7 +1,12 @@
 package com.ballistic.audioshadow.domain.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class AudioRecording(
+    @PrimaryKey val id: String,
+    val title: String,
     val filePath: String,
-    val duration: Long,
-    val timestamp: Long
+    val duration: Long
 )
